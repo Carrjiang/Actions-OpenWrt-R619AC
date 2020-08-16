0c900de9dd5d0a73c8b39cb278c456f76dc7f822
Carrjiang <736140506@qq.com>
2020-08-16 21:04:15 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,12 +9,12 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-cd package/lean/
-git clone https://github.com/jerrykuku/node-request.git
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git
-
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+cd package/lean/
+git clone https://github.com/jerrykuku/node-request.git
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git
